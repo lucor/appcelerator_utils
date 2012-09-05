5ea0ee8e56ab9d1b0d87a7c250fcb7652b2e8824
lucor <lu.corbo@gmail.com>
2012-09-05 10:21:37 +0200

    Improved code

--- run_ipad_simulator.sh
+++ run_ipad_simulator.sh
@@ -5,6 +5,7 @@ app_name='your_app_name'
 ios_sdk_version='5.1'
 ti_version="2.1.2.GA"
 
-alias ios_builder="~/Library/Application\ Support/Titanium/mobilesdk/osx/$ti_version/iphone/builder.py"
+ios_builder_path="$HOME/Library/Application Support/Titanium/mobilesdk/osx/$ti_version/iphone"
 
-ios_builder run "`pwd`" $ios_sdk_version $app_id $app_name ipad
+"${ios_builder_path}"/builder.py run "`pwd`" $ios_sdk_version $app_id $app_name ipad
+
